3fa0d6ff9b0f3f3ad3f1f36884a66d29dd99a6b2
Leonid7777 <90553178+Leonid7777@users.noreply.github.com>
2024-12-06 22:36:13 +0300

    Update remiz.py

--- remiz.py
+++ remiz.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.optimize import newton
 
 
 def F(x):
@@ -10,7 +11,7 @@ def p(x, mas):
         val += mas[i] * x**i
     return val
 
-def maximize_scan(p_coef, a, b, num_points=10):
+def maximize_scan(p_coef, a, b, num_points=1000):
     x = np.linspace(a, b, num_points)
     y = np.array([np.abs(F(val) - p(val, p_coef)) for val in x])
     max_index = np.argmax(y)
@@ -18,7 +19,7 @@ def maximize_scan(p_coef, a, b, num_points=10):
 
 
 eps = 10**(-2)
-deg = 2
+deg = 3
 a, b = -1, 1
 
 points = np.sort(np.random.rand(deg + 2) * (b - a) + a)
@@ -36,47 +37,32 @@ while err > eps:
 
     new_point = maximize_scan(p_coef, a, b)
 
+    func_res = F(new_point) - p(new_point, p_coef)
+
     ind = np.argwhere(points > new_point)
     if ind.shape[0]:
         ind = ind.min()
+        if ind == 0 and vander[ind, -1] * d * func_res < 0:
+            vander[1:, :] = vander[:-1, :]
+            F_val[1:] = F_val[:-1]
+            vander[ind, -1] = -vander[ind, -1]
+        elif vander[ind, -1] * d * func_res < 0:
+                ind -= 1
     else:
-        ind = deg + 2
+        ind = deg + 1
+        if vander[ind, -1] * d * func_res < 0:
+            vander[:-1, :] = vander[1:, :]
+            F_val[:-1] = F_val[1:]
+            vander[ind, -1] = -vander[ind, -1]
 
-    func_res = F(new_point) - p(new_point, p_coef)
+    vander[ind, :-1] = np.array([new_point**i for i in range(deg, -1, -1)])
+    F_val[ind] = F(new_point)
 
     err = np.abs(np.abs(func_res) - np.abs(d))
 
-    if ind == 0:
-        if vander[ind, -1] * d * func_res >= 0:
-            vander[ind, :-1] = np.array([new_point**i for i in range(deg, -1, -1)])
-            F_val[ind] = F(new_point)
-        else:
-            vander[1:, :] = vander[:-1, :]
-            F_val[1:] = F_val[:-1]
-            vander[ind, :-1] = np.array([new_point**i for i in range(deg, -1, -1)])
-            vander[ind, -1] = -vander[ind, -1]
-            F_val[ind] = F(new_point)
-    elif ind == deg + 2:
-        ind -= 1
-        if vander[ind, -1] * d * func_res >= 0:
-            vander[ind, :-1] = np.array([new_point**i for i in range(deg, -1, -1)])
-            F_val[ind] = F(new_point)
-        else:
-            vander[:-1, :] = vander[1:, :]
-            F_val[:-1] = F_val[1:]
-            vander[ind, :-1] = np.array([new_point**i for i in range(deg, -1, -1)])
-            vander[ind, -1] = -vander[ind, -1]
-            F_val[ind] = F(new_point)
-    else:
-        if vander[ind, -1] * d * func_res >= 0:
-            vander[ind, :-1] = np.array([new_point**i for i in range(deg, -1, -1)])
-            F_val[ind] = F(new_point)
-        else:
-            ind -= 1
-            vander[ind, :-1] = np.array([new_point**i for i in range(deg, -1, -1)])
-            F_val[ind] = F(new_point)
-
-    print(np.abs(func_res))
+    print(f'err = {err}')
+    print(f'func_res = {func_res}')
+    print()
 
 
-print(err)
+print(f'coef of polinom = {p_coef}')
