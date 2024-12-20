23aa627051415fca879a578e443edad46e100ac1
Leonid7777 <90553178+Leonid7777@users.noreply.github.com>
2024-12-21 01:31:57 +0300

    Update monte_carlo.py

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,7 +1,6 @@
 import numpy as np
 from scipy.integrate import quad
-import sobol_seq
-
+from scipy.stats import qmc
 
 def func(x):
     # return 5 * x + 7
@@ -41,39 +40,38 @@ def monte_carlo(start_point, end_point, tolerance, function):
 
     test(start_point, end_point, function, tolerance, first_val)
 
-def geomic_monte_carlo(start_point, end_point, tolerance, function, max_val, grid):
+def geomic_monte_carlo(start_point, end_point, tolerance, function, max_val, grid, sigma):
 
-    count_of_point = 1000
     length = end_point - start_point
 
     volume = length * max_val
 
     first_val = 0
-    second_val = 2 * tolerance 
 
-    while np.abs(first_val - second_val) >= tolerance / 2:
-        if grid == "Uniform":
-            points_x = start_point + np.random.rand(count_of_point) * length
-            points_y = np.random.rand(count_of_point) * max_val
-        elif grid == "Sobol":
-            points = sobol_seq.i4_sobol_generate(2, count_of_point)
-            points_x = [x[0] * length + start_point for x in points]
-            points_y = [x[1] * max_val for x in points]
-        else:
-            points = sobol_seq.i4_sobol_generate(2, count_of_point)
-            points_x = [x[0] * length + start_point for x in points]
-            points_y = [x[1] * max_val for x in points]
-            np.random.shuffle(points_y)
+    count_of_point = int(np.ceil( (sigma * length / tolerance)**2 / 12) )
 
-        second_val = first_val
-        first_val = 0
+    print(count_of_point)
 
-        for i in range(count_of_point):
-            first_val += (function(points_x[i]) >= points_y[i])
+    if grid == "Uniform":
+        points_x = start_point + np.random.rand(count_of_point) * length
+        points_y = np.random.rand(count_of_point) * max_val
+    elif grid == "Sobol":
+        sampler = qmc.Sobol(d=2, scramble=False)
+        points = sampler.random(count_of_point)
+        points_x = [x[0] * length + start_point for x in points]
+        points_y = [x[1] * max_val for x in points]
+    else:
+        sampler = qmc.Sobol(d=2, scramble=True)
+        points = sampler.random(count_of_point)
+        points_x = [x[0] * length + start_point for x in points]
+        points_y = [x[1] * max_val for x in points]
 
-        first_val *= volume / count_of_point
+    first_val = 0
 
-        count_of_point *= 10
+    for i in range(count_of_point):
+        first_val += (function(points_x[i]) >= points_y[i])
+
+    first_val *= volume / count_of_point
 
     test(start_point, end_point, function, tolerance, first_val)
 
@@ -81,10 +79,11 @@ start_point = 0.1
 end_point = 2
 tolerance = 5 * 10**(-2)
 max_val = np.exp(3) 
+sigma = 5
 method = "geomic_monte_carlo"
-grid = "Sobol_shf"
+grid = "Sobol"
 
 if method == "monte_carlo":
     monte_carlo(start_point, end_point, tolerance, func)
 elif method == "geomic_monte_carlo":
-    geomic_monte_carlo(start_point, end_point, tolerance, func, max_val, grid)
+    geomic_monte_carlo(start_point, end_point, tolerance, func, max_val, grid, sigma)
